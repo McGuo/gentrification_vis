ff32265d7b237097dd354c296384c5a67571a2ca
Michael Guo <michaelgguo@gmail.com>
2019-09-01 16:31:29 -0700

    working database v1

--- yelp.js
+++ yelp.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const yelp = require("yelp-fusion");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
-const jsonexport = require("jsonexport");
+const client = yelp.client(process.env.CLIENT_SECRET);
 
 const csvWriter = createCsvWriter({
-  path: "dummy.csv",
+  path: "database/food.csv",
   header: [
     { id: "id", title: "id" },
     { id: "alias", title: "alias" },
@@ -19,41 +19,64 @@ const csvWriter = createCsvWriter({
     { id: "phone", title: "phone" },
     { id: "latitude", title: "latitude" },
     { id: "longitude", title: "longitude" },
-    { id: "address", title: "address" }
-  ]
+    { id: "address", title: "address" },
+    { id: "offset", title: "offset" }
+  ],
+  append: "true"
 });
 
-const client = yelp.client(process.env.CLIENT_SECRET);
+const writeDatabase = () => {};
 
-client
-  .search({
-    location: "san francisco, ca"
-  })
-  .then(response => {
-    // console.log(response.jsonBody.businesses[0]);
-    //
-    let data = response.jsonBody.businesses;
-
-    console.log(data[0]);
-
-    data.forEach(element => {
-      element.longitude = element.coordinates.longitude;
-      element.latitude = element.coordinates.latitude;
-      element.address = element.location.display_address;
-      let string = "";
-
-      element.categories.forEach(category => {
-        string = `${string}${category.alias};`;
-      });
-      element.categories = string;
-    });
-
-    csvWriter
-      .writeRecords(data) // returns a promise
-      .then(() => {
-        console.log("...Done");
-      });
-  })
-  .catch(e => {
+const yelpAPILoop = async (location, term, limit) => {
+  console.log(`Location: ${location}`);
+  console.log(`Term: ${term}`);
+  try {
+    for (let offset = 0; offset <= 1000; offset = offset + 50) {
+      console.log(`We are currently at page: ${Math.round(offset / 50)}`);
+      let restaurants = await yelpAPISearch(location, term, limit, offset);
+      await writeToCSV(restaurants);
+    }
+  } catch (e) {
     console.log(e);
+    return;
+  }
+};
+
+const writeToCSV = async data => {
+  await csvWriter.writeRecords(data);
+  console.log("Successfully written to database");
+};
+
+const yelpAPISearch = async (location, term, limit, offset) => {
+  let response = await client.search({ location, term, limit, offset });
+  let data = response.jsonBody.businesses;
+  data.forEach(element => {
+    element.longitude = element.coordinates.longitude;
+    element.latitude = element.coordinates.latitude;
+    element.address = element.location.display_address;
+    element.offset = offset;
+    let string = "";
+
+    element.categories.forEach(category => {
+      string = `${string}${category.alias};`;
+    });
+    element.categories = string;
   });
+
+  console.log(data);
+
+  return data;
+};
+
+wait = async delay => {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, randomNumber(delay, delay * 2));
+  });
+};
+randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+// yelpAPISearch("san francisco", "food", 50, 10);
+
+yelpAPILoop("san francisco, ca", "food", 50);
