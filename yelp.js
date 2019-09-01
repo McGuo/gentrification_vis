791751bb8552ae8677df5f3c8cdcaf97513dcc80
Michael Guo <michaelgguo@gmail.com>
2019-09-01 14:46:09 -0700

    working v1 with dummy data for jj

--- yelp.js
+++ yelp.js
@@ -1,5 +1,27 @@
 require("dotenv").config();
 const yelp = require("yelp-fusion");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const jsonexport = require("jsonexport");
+
+const csvWriter = createCsvWriter({
+  path: "dummy.csv",
+  header: [
+    { id: "id", title: "id" },
+    { id: "alias", title: "alias" },
+    { id: "name", title: "name" },
+    { id: "image_url", title: "image_url" },
+    { id: "url", title: "url" },
+    { id: "review_count", title: "review_count" },
+    { id: "categories", title: "categories" },
+    { id: "rating", title: "rating" },
+    { id: "transactions", title: "transactions" },
+    { id: "price", title: "price" },
+    { id: "location", title: "location" },
+    { id: "phone", title: "phone" },
+    { id: "latitude", title: "latitude" },
+    { id: "longitude", title: "longitude" }
+  ]
+});
 
 const client = yelp.client(process.env.CLIENT_SECRET);
 
@@ -8,7 +30,20 @@ client
     location: "san francisco, ca"
   })
   .then(response => {
-    console.log(response.jsonBody.businesses);
+    // console.log(response.jsonBody.businesses[0]);
+    //
+    let data = response.jsonBody.businesses;
+
+    data.forEach(element => {
+      element.longitude = element.coordinates.longitude;
+      element.latitude = element.coordinates.latitude;
+    });
+
+    csvWriter
+      .writeRecords(data) // returns a promise
+      .then(() => {
+        console.log("...Done");
+      });
   })
   .catch(e => {
     console.log(e);
